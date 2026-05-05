79db6d04d5473cc772dc282387f3e46ab93a6757
ClaraBerglundHiltunen <clara.berglund.hiltunen@gmail.com>
2026-05-05 02:38:31 +0200

    python fibonacci runs

--- java_code/src/algorithms/main.java
+++ java_code/src/algorithms/main.java
@@ -4,8 +4,8 @@ public class main {
 
 	public static void main(String[] args) {
 		Fibonacci f = new Fibonacci();
-		for(int i = 0; i < 1; i++) {
-			f.calcFibonacci(45);
+		for(int i = 0; i < 1000; i++) {
+			f.calcFibonacci(40);
 		}
 
 
---
+++ python_code/fibonacci.py
@@ -0,0 +1,22 @@
+import time
+file_out = open(r"C:\Users\clara\OneDrive\Dokument\LTH\Utvärdering_av_programvarusystem\uaps-project\python_fib_times.txt","+a")
+
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+n = 35
+
+for i in range(1000):
+    t1 = time.perf_counter_ns()
+    fib(n)
+    t2 = time.perf_counter_ns()
+    delta = t2-t1
+    file_out.write(str(n) + "," + str(delta)+ "\n")
+    print(i)
+    
