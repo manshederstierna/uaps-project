6e31ce232835c86ae60ec40ddae1b3fb1e2e4c8e
manshederstierna <manshederstierna2006@gmail.com>
2026-05-07 14:21:08 +0200

    Integer sorting data done (5000) measurements

--- java_code/src/algorithms/IntegerSorter.java
+++ java_code/src/algorithms/IntegerSorter.java
@@ -28,7 +28,7 @@ public class IntegerSorter {
 	
 	
 	public void insertionSort() {
-		for(int i=0; i 	< numbers.size(); i++) {
+		for(int i=1; i 	< numbers.size(); i++) {
 			int key = numbers.get(i);
 			int j = i-1;
 	
@@ -42,11 +42,26 @@ public class IntegerSorter {
 	}
 
 	public long timeToSort() throws IOException {
-		this.readData();
 		long t1 = System.nanoTime();
 		this.insertionSort();
 		long t2 = System.nanoTime();
 		return t2 - t1;
+	}
+
+	public void clearArray(){
+		this.numbers.clear();
+	}
+
+	public static void write_to_file(String s, int n,String path) {
+		try {
+			FileWriter myWriter = new FileWriter(path,true);
+			myWriter.write(n + "," + s + "\n");
+			myWriter.close();
+			System.out.println("Successfully wrote to the file.");
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 	}
 	
 	
--- java_code/src/algorithms/main.java
+++ java_code/src/algorithms/main.java
@@ -1,18 +1,27 @@
 package algorithms;
 
+import java.io.IOException;
+
 public class main {
 
-	public static void main(String[] args) {
-		Fibonacci f = new Fibonacci();
+	public static void main(String[] args) throws IOException {
+		/* Fibonacci f = new Fibonacci();
 		for(int i = 0; i < 1000; i++) {
 			f.calcFibonacci(40);
-		}
+		} */
 		
-		String path = "";
-		IntegerSorter intsorter = new IntegerSorter(path);
+		String path = "/home/manshederstierna/gitrepos/uaps-project/data.txt";
+		String outpath = "/home/manshederstierna/gitrepos/uaps-project/java_sort_times.txt";
+		for(int i=0; i < 5000; i++){
+			IntegerSorter intsorter = new IntegerSorter(path);
+			intsorter.readData();
+			String time = Long.toString(intsorter.timeToSort());
+			intsorter.write_to_file(time,i,outpath);
+			intsorter.clearArray();
+		}
 		// TODO
-
-
 	}
+
+	
 
 }
